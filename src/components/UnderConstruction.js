93854fc279370b9dd391cb0a0134b76a5b7c1898
Divyansh Singh <40380293+brc-dd@users.noreply.github.com>
2020-10-22 22:04:55 +0530

    feat: add under-construction page

--- src/components/UnderConstruction.js
+++ src/components/UnderConstruction.js
@@ -1,5 +1,20 @@
 import React from 'react';
 
+import 'assets/css/UnderConstruction.css';
+import { ReactComponent as Gear } from 'assets/images/gear.svg';
+
 export default function UnderConstruction() {
-  return <>this website is under construction</>;
+  return (
+    <>
+      <div className="warning-content">
+        <h1>New Site Under Construction</h1>
+        <Gear />
+        <p>
+          Please forgive the inconvenience. <br />
+          We are currently initializing our brand new site.
+        </p>
+        <p>It's okay, we're excited too!</p>
+      </div>
+    </>
+  );
 }
