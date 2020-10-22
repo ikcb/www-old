96cbc134388a66ce1001fd9ecbe76e685c6b1632
Divyansh Singh <40380293+brc-dd@users.noreply.github.com>
2020-10-22 12:12:15 +0530

    feat: lay out project structure

---
+++ src/components/UnderConstruction.js
@@ -0,0 +1,5 @@
+import React from 'react';
+
+export default function UnderConstruction() {
+  return <>this website is under construction</>;
+}
--- src/index.js
+++ src/index.js
@@ -1,17 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+
+// import global css files here
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+// import user defined components here
+import UnderConstruction from 'components/UnderConstruction';
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <Router>
+    <Switch>
+      <Route exact path="/" component={UnderConstruction} />
+    </Switch>
+  </Router>,
   document.getElementById('root')
 );
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
