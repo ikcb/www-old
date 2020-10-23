d1a6a1222f06cbaa7ac26c8a64436563d57a97dd
Divyansh Singh <40380293+brc-dd@users.noreply.github.com>
2020-10-23 14:36:34 +0530

    fix: remove unused styles

--- src/components/UnderConstruction.js
+++ src/components/UnderConstruction.js
@@ -1,20 +1,19 @@
 import React from 'react';
 
-import 'assets/css/UnderConstruction.css';
 import { ReactComponent as Gear } from 'assets/images/gear.svg';
 
 export default function UnderConstruction() {
   return (
-    <>
-      <div className="warning-content">
-        <h1>New Site Under Construction</h1>
-        <Gear />
-        <p>
+    <div className="jumbotron jumbotron-fluid d-flex align-items-center vh-100 p-0 m-0">
+      <div className="container-fluid text-center">
+        <h1 className="display-4 pb-4 mb-4">New Site Under Construction</h1>
+        <Gear style={{ transform: 'scale(1.28)' }} />
+        <p className="lead pt-4 mt-4">
           Please forgive the inconvenience. <br />
           We are currently initializing our brand new site.
         </p>
-        <p>It's okay, we're excited too!</p>
+        <p className="lead">It's okay, we're excited too!</p>
       </div>
-    </>
+    </div>
   );
 }
