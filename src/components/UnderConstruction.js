a5047e18ef2d89077d2ef75a115fd5c429e5973e
Divyansh Singh <40380293+brc-dd@users.noreply.github.com>
2020-10-25 00:31:30 +0530

    fix: make heading smaller in size

--- src/components/UnderConstruction.js
+++ src/components/UnderConstruction.js
@@ -17,7 +17,9 @@ export default function UnderConstruction() {
   return (
     <div ref={el} className="jumbotron d-flex align-items-center m-0">
       <div className="container-fluid text-center">
-        <h1 className="display-4 pb-4 mb-4">New Site Under Construction</h1>
+        <h1 className="h1 font-weight-light pb-4 mb-4">
+          New Site Under Construction
+        </h1>
         <Gear style={{ transform: 'scale(1.28)' }} />
         <p className="lead pt-4 mt-4">
           Please forgive the inconvenience. <br />
