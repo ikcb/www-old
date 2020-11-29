4ddeb8687cfd8e84b5264fe47b0987fe0af93265
Divyansh Singh <40380293+brc-dd@users.noreply.github.com>
2020-11-29 20:37:06 +0530

    fix: done some design refactoring

--- src/components/TitleBrand.js
+++ src/components/TitleBrand.js
@@ -2,11 +2,21 @@ import 'assets/styles/_titleBrand.scss';
 
 import $ from 'jquery';
 import { debounce } from 'lodash';
-import React, { useEffect, useState } from 'react';
-import { Col, Container, Jumbotron, Row } from 'react-bootstrap';
+import React, { useEffect, useLayoutEffect, useRef, useState } from 'react';
+import { Button, Col, Container, Jumbotron, Row } from 'react-bootstrap';
+import { Link } from 'react-scroll';
 import { Waypoint } from 'react-waypoint';
+import useWindowDimensions from 'utils/WindowDimensions';
 
 export default function TitleBrand() {
+  const el = useRef();
+  const { height, width } = useWindowDimensions();
+
+  useLayoutEffect(() => {
+    if (el.current.scrollHeight <= height) el.current.classList.add('vh-100');
+    else el.current.classList.remove('vh-100');
+  });
+
   const handleNavbarOnEnter = () => {
     $('.navbar').addClass('navbar-dark bg-transparent');
     $('.navbar').removeClass('navbar-light bg-light');
@@ -42,23 +52,49 @@ export default function TitleBrand() {
   }, []);
 
   return (
-    <Container fluid className="bg title-brand vh-100">
-      <Row className="align-items-center h-100">
-        <Col>
-          <Jumbotron className="bg-transparent text-light text-center">
-            <Waypoint
-              onEnter={handleNavbarOnEnter}
-              onLeave={handleNavbarOnLeave}
-              topOffset={TitlePositionFromTop}
-            >
-              <h1 className="display-0">IIIT Kota CodeBase</h1>
-            </Waypoint>
-            <h2 className="lead mt-4">
-              The Free and Open Source Society of IIIT Kota
-            </h2>
-          </Jumbotron>
-        </Col>
-      </Row>
+    <Container fluid className="bg title-brand" ref={el}>
+      <Container fluid="lg" className="h-100">
+        <Row className="align-items-center h-100">
+          <Col lg={7}>
+            <Jumbotron className="bg-transparent text-light text-center text-lg-left">
+              <Waypoint
+                onEnter={handleNavbarOnEnter}
+                onLeave={handleNavbarOnLeave}
+                topOffset={TitlePositionFromTop}
+              >
+                <h1 className="display-0">IIIT Kota CodeBase</h1>
+              </Waypoint>
+              <h2 className="lead mt-4">
+                The Free and Open Source Society of IIIT Kota
+              </h2>
+              <Link
+                to="about"
+                smooth={true}
+                duration={500}
+                offset={width < 992 ? -78 : -66}
+              >
+                <Button
+                  className="mt-5 mr-sm-3 rounded-0"
+                  variant="outline-light"
+                  size="lg"
+                  id="btn-1"
+                >
+                  Discover More
+                </Button>
+              </Link>
+              <br className="d-sm-none" />
+              <Button
+                className="mt-3 mt-sm-5 ml-sm-3 rounded-0"
+                variant="outline-light"
+                size="lg"
+                id="btn-2"
+              >
+                Join the Team
+              </Button>
+            </Jumbotron>
+          </Col>
+        </Row>
+      </Container>
     </Container>
   );
 }
