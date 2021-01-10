44e7fd957ead631b0c115f143f18548344db5446
Divyansh Singh <40380293+brc-dd@users.noreply.github.com>
2021-01-10 19:07:01 +0530

    feat: finshed adding animations

--- src/components/IndexFooter.js
+++ src/components/IndexFooter.js
@@ -10,30 +10,36 @@ import {
   AiOutlineMedium
 } from 'react-icons/ai';
 import { FaDiscord } from 'react-icons/fa';
+import Bounce from 'react-reveal/Bounce';
+import Fade from 'react-reveal/Fade';
 
 export default function IndexFooter() {
   return (
     <footer>
       <Container>
         <Row className="justify-content-center pt-5">
-          <a href="https://github.com/iiitkota-codebase">
-            <AiOutlineGithub />
-          </a>
-          <a href="https://instagram.com/iiitkota_codebase">
-            <AiOutlineInstagram />
-          </a>
-          <a href="mailto:codebase@iiitkota.tech">
-            <AiOutlineMail />
-          </a>
-          <a href="https://medium.com/codebase-iiitkota">
-            <AiOutlineMedium />
-          </a>
-          <a href="https://discord.com/invite/ddaskT9kaM">
-            <FaDiscord />
-          </a>
+          <Bounce>
+            <a href="https://github.com/iiitkota-codebase">
+              <AiOutlineGithub />
+            </a>
+            <a href="https://instagram.com/iiitkota_codebase">
+              <AiOutlineInstagram />
+            </a>
+            <a href="mailto:codebase@iiitkota.tech">
+              <AiOutlineMail />
+            </a>
+            <a href="https://medium.com/codebase-iiitkota">
+              <AiOutlineMedium />
+            </a>
+            <a href="https://discord.com/invite/ddaskT9kaM">
+              <FaDiscord />
+            </a>
+          </Bounce>
         </Row>
         <Row className="text-secondary justify-content-center pt-4 pb-5">
-          © 2021 IIIT Kota CodeBase
+          <Fade delay={400} duration={1600}>
+            © 2021 IIIT Kota CodeBase
+          </Fade>
         </Row>
       </Container>
     </footer>
