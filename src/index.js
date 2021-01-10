b0ceecdbb3b3d626f9c42f5dfb11f80f59bff45e
Divyansh Singh <40380293+brc-dd@users.noreply.github.com>
2021-01-10 19:14:28 +0530

    chore: take site out of beta

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import 'assets/styles/_index.scss';
 
 import HomePage from 'components/HomePage';
-import UnderConstruction from 'components/UnderConstruction';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Route, BrowserRouter as Router, Switch } from 'react-router-dom';
@@ -9,8 +8,7 @@ import { Route, BrowserRouter as Router, Switch } from 'react-router-dom';
 ReactDOM.render(
   <Router>
     <Switch>
-      <Route exact path="/" component={UnderConstruction} />
-      <Route path="/beta" component={HomePage} />
+      <Route exact path="/" component={HomePage} />
     </Switch>
   </Router>,
   document.getElementById('root')
